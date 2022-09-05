dda58d388bc75e0515821010b5353204838b588e
raakostOnCph <36969041+raakostOnCph@users.noreply.github.com>
2022-09-05 13:27:21 +0200

    laver udskriv

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,31 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
+     Node node1 = new Node("hej");
+     Node node2 = new Node("med");
+     Node node3 = new Node("dig");
+
+     node1.next = node2;
+     node2.next = node3;
+
+     node3.previous = node2;
+     node2.previous = node1;
+
+     Node n = node1;
+
+        printFromHead(n);
 
 
+    }
 
+    private static void printFromHead(Node n)
+    {
+        while (n != null) {
+
+            System.out.println(n.data);
+
+            n = n.next;
+        }
     }
 
 
